946891aa7f86eeba1af26c5a0a4ec9750bb7deab
yacineb <ybsoft2011@live.com>
2024-12-09 14:37:32 +0100

    build: emscripten flags , main function for wasm module

--- pgrx-examples/world/src/main.rs
+++ pgrx-examples/world/src/main.rs
@@ -1,14 +1,9 @@
 #![allow(non_snake_case)]
-#![feature(start)]
 
 #[no_mangle]
 pub fn PG_version() {
     println!("Hello I'm pglite");
 }
-
-
-#[start]
-fn start(_argc: isize, _argv: *const *const u8) -> isize {
-
-    0
+fn main() {
+    // nothing needed here
 }
